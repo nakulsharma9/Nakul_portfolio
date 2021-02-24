59a566d6486fc18477c3d782f4f1bbff15b684d7
Nakul Sharma <74112608+nakulsharma9@users.noreply.github.com>
2021-02-24 12:16:04 +0530

    optimised photos

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 $(window).scroll(function () {
     if ($(window).scrollTop() > 400) {
         $("a.tbtn").fadeIn();
-    }
-    else {
+    } else {
         $("a.tbtn").fadeOut();
     }
 });
 $("a.tbtn").click(function () {
-    $("html").animate({ scrollTop: 0 })
+    $("html").animate({
+        scrollTop: 0
+    })
 })
 document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
         document.querySelector("content").style.visibility = "hidden";
         document.querySelector("#loader").style.visibility = "visible";
-    }
-    else {
+    } else {
         document.querySelector("#loader").style.display = "none";
         document.querySelector("content").style.visibility = "visible";
     }
-}; 
+};
