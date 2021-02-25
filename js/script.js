cc3c92906471ee52631e4323904de0eb8eacbe2b
Nakul Sharma <74112608+nakulsharma9@users.noreply.github.com>
2021-02-25 20:10:39 +0530

    added responsive of ipad and iphone 6

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ $("a.tbtn").click(function () {
         scrollTop: 0
     })
 })
+let hamburger = document.querySelector('.hamburger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', function () {
+    mobileNav.classList.add('open');
+})
+times.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+
+})
